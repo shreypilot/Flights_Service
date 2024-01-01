bf83e9c6bb414e306006adfed563be6bb17960f3
shreypilot <shreypilot28@gmail.com>
2024-01-01 15:51:05 +0530

    Added basic sequelise and cli related files

---
+++ src/controllers/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    InfoController: require('./info-controller'),
+    AirplaneController: require('./airplane-controller'),
+    CityController: require('./flight-controller'),
+    AirportController: require('./airport-controller'),
+    FlightController: require('./city-controller')
+}
