25214702ea190560e0ac842c3e87fb32e704f7a6
shreypilot <shreypilot28@gmail.com>
2024-01-07 13:04:03 +0530

    initial setup with one CRUD API done

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,6 +1,3 @@
 module.exports = {
     AirplaneRepository: require('./airplane-repository'),
-    CityRepository: require('./city-repository'),
-    AirportRepository: require('./airport-respository'),
-    FlightRepository: require('./flight-repository')
 }
