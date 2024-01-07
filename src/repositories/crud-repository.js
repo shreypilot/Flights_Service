342be7d5a7b127f4ebe83d33f4b9625d867709fe
shreypilot <shreypilot28@gmail.com>
2024-01-07 15:26:43 +0530

    Added get airplane api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -1,5 +1,6 @@
 const { Logger } = require('../config');
-
+const { StatusCodes } = require('http-status-codes');
+const AppError = require('../utils/errors/app-error');
 class CrudRepository {
     constructor(model) {
         this.model = model;
@@ -21,6 +22,9 @@ class CrudRepository {
 
     async get(data) {
         const response = await this.model.findByPk(data);
+        if(!response) {
+            throw new AppError('Not able to fund the resource', StatusCodes.NOT_FOUND);
+        }
         return response;
     }
 
