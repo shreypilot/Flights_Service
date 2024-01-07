5c7cfe4e88258d1c54f386309a96ed04235c6756
shreypilot <shreypilot28@gmail.com>
2024-01-07 15:34:18 +0530

    added delete airplane api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -17,6 +17,9 @@ class CrudRepository {
                 id: data
             }
         });
+        if(!response) {
+            throw new AppError('Not able to fund the resource', StatusCodes.NOT_FOUND);
+        }
         return response;
     }
 
